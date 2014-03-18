fff5b7c003468f6a958b9ef5c9db39c0a006e771
Frank Meier (Mac) <frank.meier@unl.edu>
2014-03-18 15:30:04 -0500

    Bestimmung von r implementiert

--- detDisk.C
+++ detDisk.C
@@ -1,15 +1,32 @@
+#include "TVector3.h"
+#include "TMath.h"
+
 #include "detDisk.h"
 
 DetDisk::DetDisk(double z, double r_min, double r_max)
 {
     _vec_min.SetXYZ(r_max, 0, z); // the larger r gives the lower eta value
     _vec_max.SetXYZ(r_min, 0, z);
+    calcEtaBoundaries();
+}
+
+void DetDisk::calcEtaBoundaries()
+{
     _eta_min = _vec_min.Eta();
     _eta_max = _vec_max.Eta();
 }
 
-DetDisk::calcEtaBoundaries()
+bool DetDisk::hit(double eta, double &r, double &z)
 {
-
+    r = 0;
+    z = 0;
+    if (eta >= _eta_min && eta <= _eta_max)
+    {
+	z = _vec_min.z();
+	const double tantheta = TMath::Tan(2*TMath::ATan(TMath::Exp(-eta)));
+	r = z * tantheta;
+    }
+    else
+	return false;
 }
 
