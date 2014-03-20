076794a8fb2b9c6b65cc53d2328cf955ebdbbf24
Frank Meier (Mac) <frank.meier@unl.edu>
2014-03-19 23:05:17 -0500

    Strawman 2

--- demo1.C
+++ demo1.C
@@ -35,27 +35,31 @@ void demo1()
     */
 
     // Variante mit 3 verschiedenen Diskgeometrien
-    { Det d; d.setDisk( 100.0, 13.5, 15.0, 56, true); trk.add(d); }
-    { Det d; d.setDisk( 111.0, 13.5, 15.0, 56, true); trk.add(d); }
-    { Det d; d.setDisk( 123.0, 13.5, 15.0, 56, true); trk.add(d); }
-    { Det d; d.setDisk( 136.0, 13.5, 15.0, 56, true); trk.add(d); }
-    { Det d; d.setDisk( 150.0, 13.5, 15.0, 56, true); trk.add(d); }
-    { Det d; d.setDisk( 166.0, 13.5, 15.0, 56, true); trk.add(d); }
-    { Det d; d.setDisk( 184.0, 13.5, 15.0, 56, true); trk.add(d); }
-    { Det d; d.setDisk( 200.0, 13.5, 15.0, 56, true); trk.add(d); }
-    { Det d; d.setDisk( 220.0, 13.5, 15.0, 56, true); trk.add(d); }
-    { Det d; d.setDisk( 243.0, 13.5, 15.0, 56, true); trk.add(d); }
+    { Det d; d.setDisk( 100.0, 14.5, 16.0, 56, true); trk.add(d); } // 1
+    { Det d; d.setDisk( 109.0, 14.5, 16.0, 56, true); trk.add(d); }
+    { Det d; d.setDisk( 118.0, 14.5, 16.0, 56, true); trk.add(d); }
+    { Det d; d.setDisk( 128.0, 14.5, 16.0, 56, true); trk.add(d); }
+    { Det d; d.setDisk( 140.0, 14.5, 16.0, 56, true); trk.add(d); } // 5
+    { Det d; d.setDisk( 153.0, 14.5, 16.0, 56, true); trk.add(d); }
+    { Det d; d.setDisk( 168.0, 14.5, 16.0, 56, true); trk.add(d); }
+    { Det d; d.setDisk( 184.0, 14.5, 16.0, 56, true); trk.add(d); }
+    { Det d; d.setDisk( 202.0, 14.5, 16.0, 56, true); trk.add(d); }
+    { Det d; d.setDisk( 222.0, 14.5, 16.0, 56, true); trk.add(d); } // 10
+    { Det d; d.setDisk( 240.0, 14.5, 16.0, 56, true); trk.add(d); }
 
-    { Det d; d.setDisk( 76.0, 5.5, 12.0, 56, true); trk.add(d); }
-    { Det d; d.setDisk( 120.0, 5.5, 12.0, 56, true); trk.add(d); }
-    { Det d; d.setDisk( 190.0, 5.5, 12.0, 56, true); trk.add(d); }
+    { Det d; d.setDisk( 66.0, 5.5, 12.0, 56, true); trk.add(d); }
+    { Det d; d.setDisk( 88.0, 5.5, 12.0, 56, true); trk.add(d); }
+    { Det d; d.setDisk( 130.0, 5.5, 12.0, 56, true); trk.add(d); }
+    { Det d; d.setDisk( 180.0, 5.5, 12.0, 56, true); trk.add(d); }
 
-    { Det d; d.setDisk( 60.0, 3.2, 4.8, 56, true); trk.add(d); }
-    { Det d; d.setDisk( 88.0, 3.2, 4.8, 56, true); trk.add(d); }
-    { Det d; d.setDisk( 120.0, 3.2, 4.8, 56, true); trk.add(d); }
-    { Det d; d.setDisk( 150.0, 3.2, 4.8, 56, true); trk.add(d); }
-    { Det d; d.setDisk( 178.0, 3.2, 4.8, 56, true); trk.add(d); }
-    { Det d; d.setDisk( 208.0, 3.2, 4.8, 56, true); trk.add(d); }
+    { Det d; d.setDisk( 34.0, 3.2, 4.8, 56, true); trk.add(d); }
+    { Det d; d.setDisk( 48.0, 3.2, 4.8, 56, true); trk.add(d); }
+    { Det d; d.setDisk( 65.0, 3.2, 4.8, 56, true); trk.add(d); }
+    { Det d; d.setDisk( 84.0, 3.2, 4.8, 56, true); trk.add(d); }
+    { Det d; d.setDisk( 112.0, 3.2, 4.8, 56, true); trk.add(d); }
+    { Det d; d.setDisk( 138.0, 3.2, 4.8, 56, true); trk.add(d); }
+    { Det d; d.setDisk( 164.0, 3.2, 4.8, 56, true); trk.add(d); }
+    { Det d; d.setDisk( 190.0, 3.2, 4.8, 56, true); trk.add(d); }
 
     { Det d; d.setDisk( 264.0, 4.5, 16.1, 56, true); trk.add(d); }
 
@@ -67,12 +71,12 @@ void demo1()
     const int N(5000);
     const double eta_step = eta_delta / N;
 
-    TH1D *h1 = new TH1D("h1", "Full tracker;#eta;# of hits", N, eta_min, eta_max);
+    TH1D *h1 = new TH1D("h1", "all pixels;#eta;# of hits", N, eta_min, eta_max);
     TH1D *h2 = new TH1D("h2", "VFPix only;#eta;# of hits", N, eta_min, eta_max);
-    TH1D *h3 = new TH1D("h3", "Full tracker;#eta;r_{min} [cm]", N, eta_min, eta_max);
-    TH1D *h4 = new TH1D("h4", "Full tracker;#eta;r_{max} [cm]", N, eta_min, eta_max);
-    TH1D *h5 = new TH1D("h5", "Full tracker;#eta;r_{max}-r_{min} [cm]", N, eta_min, eta_max);
-    TH1D *h6 = new TH1D("h6", "Full tracker;#eta;z_{min} [cm]", N, eta_min, eta_max);
+    TH1D *h3 = new TH1D("h3", "all pixels;#eta;r_{min} [cm]", N, eta_min, eta_max);
+    TH1D *h4 = new TH1D("h4", "all pixels;#eta;r_{max} [cm]", N, eta_min, eta_max);
+    TH1D *h5 = new TH1D("h5", "all pixels;#eta;r_{max}-r_{min} [cm]", N, eta_min, eta_max);
+    TH1D *h6 = new TH1D("h6", "all pixels;#eta;z_{min} [cm]", N, eta_min, eta_max);
 
     for (int i = 0; i!=N; i++)
     {
@@ -87,11 +91,13 @@ void demo1()
 	h6->SetBinContent(i+1,res.z_min);
     }
     TCanvas *c = new TCanvas("c","c", 500, 500);
+    trk.draw(0, 0, 300, 20); c->SaveAs("tracker.pdf");
     h1->Draw(); c->SaveAs("h1.pdf");
     h2->Draw(); c->SaveAs("h2.pdf");
     h3->Draw(); c->SaveAs("h3.pdf");
     h4->Draw(); c->SaveAs("h4.pdf");
     h5->Draw(); c->SaveAs("h5.pdf");
     h6->Draw(); c->SaveAs("h6.pdf");
+    trk.dump();
 }
 
